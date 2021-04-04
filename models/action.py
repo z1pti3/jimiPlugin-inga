23433d719b03214c458bb1cd9d6d512a7adfe760
z1pti3 <z1pti3@outlook.com>
2021-04-04 14:49:49 +0100

    Bug fix for new format remote

--- models/action.py
+++ models/action.py
@@ -159,8 +159,6 @@ class _ingaIPDiscoverAction(action._action):
                 if "client" in data["eventData"]["remote"]:
                     client = data["eventData"]["remote"]["client"]
                     exitCode, stdout, stderr = client.command(" ".join(["nmap","-sn","--max-rtt-timeout","800ms","--max-retries","0",scanResult.ip]),elevate=True)
-                    stdout = "\n".join(stdout)
-                    stderr = "\n".join(stderr)
                     if not stdout:
                         return { "result" : False, "rc" : 500, "msg" : stderr }
             else:
@@ -180,8 +178,6 @@ class _ingaIPDiscoverAction(action._action):
                     if "client" in data["eventData"]["remote"]:
                         client = data["eventData"]["remote"]["client"]
                         exitCode, stdout, stderr = client.command(" ".join(["nmap","--top-ports","100","-Pn","--max-rtt-timeout","800ms","--max-retries","0",scanResult.ip]),elevate=True)
-                        stdout = "\n".join(stdout)
-                        stderr = "\n".join(stderr)
                         if not stdout:
                             return { "result" : False, "rc" : 500, "msg" : stderr }
                 else:
@@ -205,8 +201,6 @@ class _ingaIPDiscoverAction(action._action):
                         if "client" in data["eventData"]["remote"]:
                             client = data["eventData"]["remote"]["client"]
                             exitCode, stdout, stderr = client.command(" ".join(["nmap","-sU","--top-ports","10","-Pn","--max-rtt-timeout","800ms","--max-retries","0",scanResult.ip]),elevate=True)
-                            stdout = "\n".join(stdout)
-                            stderr = "\n".join(stderr)
                             if not stdout:
                                 return { "result" : False, "rc" : 500, "msg" : stderr }
                     else:
@@ -281,8 +275,6 @@ class _ingaPortScan(action._action):
                     if "client" in data["eventData"]["remote"]:
                         client = data["eventData"]["remote"]["client"]
                         exitCode, stdout, stderr = client.command(" ".join(options),elevate=True)
-                        stdout = "\n".join(stdout)
-                        stderr = "\n".join(stderr)
                         if not stdout:
                             return { "result" : False, "rc" : 500, "msg" : stderr }
                 else:
