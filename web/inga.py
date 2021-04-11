74b7ce8fffdda5512a562a98e7dbf8c6656a5ece
z1pti3 <z1pti3@outlook.com>
2021-04-11 22:04:51 +0100

    Updated UI

--- web/inga.py
+++ web/inga.py
@@ -10,12 +10,16 @@ from core import api, db, storage
 from plugins.inga.models import inga
 
 from os.path import dirname, abspath, join
-from json import load
+import time
+import json
+
+import jimi
+from web import ui
 
 import random
-# pluginPages = Blueprint('ingaPages', __name__, template_folder="templates")
+pluginPages = Blueprint('ingaPages', __name__, template_folder="templates")
 
-pluginPages = Blueprint('ingaPages', __name__, template_folder="ingatemplates",static_folder="ingastatic",static_url_path="ingastatic")
+# pluginPages = Blueprint('ingaPages', __name__, template_folder="ingatemplates",static_folder="ingastatic",static_url_path="ingastatic")
 
 
 def getPortMapping(jsonData,checkVal):
@@ -48,146 +52,254 @@ def custom_static(file):
 
 @pluginPages.route("/")
 def mainPage():
+    return render_template("inga.html",CSRF=jimi.api.g.sessionData["CSRF"])
 
-    # unsecurePorts,fileTransferPorts,rdp,webserverPorts,databasePorts,networkProtoPorts,otherPort = 0,0,0,0,0,0,0
+@pluginPages.route("/barScansResults/",methods=["POST"])
+def barScansResults():
+    bar = ui.bar()
+    with open(Path("plugins/inga/web/includes/sankeyMapping.json")) as json_file:
+        serviceMap = json.load(json_file)
+    bar.addLabel("Other")
+    for service in serviceMap.keys():
+        bar.addLabel(service)
+    scansResults = inga._inga().getAsClass(api.g.sessionData,query={ "up" : True },fields=["scanName","ip","up","lastScan","ports"])
+    data = {}
+    for scansResult in scansResults:
+        if scansResult.scanName not in data:
+            data[scansResult.scanName] = { "Other" : 0 }
+            for service in serviceMap.keys():
+                data[scansResult.scanName][service] = 0
+        for port in scansResult.ports["tcp"]:
+            matched = getPortMapping(serviceMap,str(port["port"]))
+            if matched != None:
+                data[scansResult.scanName][matched] += 1
+            else:
+                data[scansResult.scanName]["Other"] += 1
+    for scan in data:
+        dataList = []
+        for k,v in data[scan].items():
+            dataList.append(v)
+        bar.addDataset(scan,dataList)
 
-    scans           = inga._inga()._dbCollection.distinct("scanName")
-    results         = []
-    sankeyChartData = [ ]
-    portsPieChart   = { }
+    data = json.loads(jimi.api.request.data)
+    return bar.generate(data), 200
 
-    jsonMappingFile = abspath(join(dirname( __file__ ), '..', 'web')) + "/includes/sankeyMapping.json" #dirname(abspath(__file__)) + "/sankeyMapping.json" #abspath(join(dirname( __file__ ), '..'))#, 'includes')) # 
-    with open(jsonMappingFile) as json_file:
-        jsonMap_data = load(json_file)
+@pluginPages.route("/tableScans/<action>/",methods=["GET"])
+def tableScans(action):
+    scans = inga._inga().groupby(sessionData=api.g.sessionData,field="scanName")
+    total = len(scans)
+    columns = [ "Scan Name", "Total Hosts" ]
+    table = ui.table(columns,total,total)
+    if action == "build":
+        return table.getColumns() ,200
+    elif action == "poll":
+        # Custom table data so it can be vertical
+        data = []
+        for source in scans:
+            data.append(["<a href=\"{0}{1}/\">{2}</a>".format("scan/",source["_id"],ui.safe(source["_id"])),ui.safe(source["_count"])])
+        table.data = data
+        return { "draw" : int(jimi.api.request.args.get('draw')), "recordsTable" : total, "recordsFiltered" : total, "recordsTotal" : total, "data" : data } ,200
 
+@pluginPages.route("/tableScansDomains/<action>/",methods=["GET"])
+def tableScansDomains(action):
+    scans = inga._inga().getAsClass(sessionData=api.g.sessionData,query={})
+    columns = [ "Domain Name", "IP", "Scan Name" ]
+    table = ui.table(columns,0,0)
+    if action == "build":
+        return table.getColumns() ,200
+    elif action == "poll":
+        data = []
+        for scan in scans:
+            for domain in scan.domains:
+                data.append([ui.safe(domain["domain"]),ui.safe(domain["ip"]),ui.safe(scan.scanName)])
+        total = len(data)
+        start = int(jimi.api.request.args.get('start'))
+        if start + 200 > len(data):
+            data = data[start:]
+        else:
+            data = data[start:start+200]
+        return { "draw" : int(jimi.api.request.args.get('draw')), "recordsTable" : len(data), "recordsFiltered" : total, "recordsTotal" : total, "data" : data } ,200
 
-    for scan in scans:
-        #reset keys for each scan
-        sankeyDict  = { "unsecurePorts": 0, "fileTransferPorts": 0, "rdp": 0, "webserverPorts": 0, "databasePorts": 0, "networkProtoPorts": 0, "otherPort": 0  }
+@pluginPages.route("/networkScansResults/",methods=["GET"])
+def networkScansResults():
+    scansResults = inga._inga().getAsClass(api.g.sessionData,query={ "up" : True },fields=["scanName","ip","up","lastScan","ports"])
 
+    nodesDict = {}
+    edgesDict = {}
+    for scansResult in scansResults:
+        if scansResult.scanName not in nodesDict:
+            nodesDict[scansResult.scanName] = { "id" : scansResult.scanName, "label" : scansResult.scanName, "shape" : "dot", "value" : 1, "color" : { "background" : "#C72F1E", "border" : "#C72F1E" , "highlight" : { "background" : "#000", "border" : "#FFF" } } }
+        if scansResult.ip not in nodesDict:
+            nodesDict[scansResult.ip] = { "id" : scansResult.ip, "label" : scansResult.ip, "shape" : "image", "image" : "/static/img/computer.svg", "value" : 1, "color" : { "background" : "#C72F1E", "border" : "#C72F1E" , "highlight" : { "background" : "#000", "border" : "#FFF" } } }
+        else:
+            nodesDict[scansResult.ip]["value"] += 1
+        key = "{0}-{1}".format(scansResult.scanName,scansResult.ip)
+        if key not in edgesDict:
+            edgesDict[key] = { "id" : key, "from" : scansResult.scanName, "to" : scansResult.ip }
+            
+    nodes = [ x for x in nodesDict.values() ]
+    edges = [ x for x in edgesDict.values() ]
+    options = {
+        "interaction": {
+            "tooltipDelay": 200,
+            "hideEdgesOnDrag": True,
+            "hideEdgesOnZoom": True,
+        },
+        "layout": {
+            "improvedLayout": False
+        },
+        "physics": {
+            "enabled": True,
+            "timestep": 1,
+            "stabilization": False,
+            "barnesHut" : {
+                "springConstant" : 0.001
+            }
+        },
+        "nodes": {
+            "shape": "dot",
+            "color": {
+                "background": "#4090c9",
+                "highlight": {
+                    "background": "#000",
+                    "border": "#FFF"
+                }
+            },
+            "font": {
+                "size": 10,
+                "face": "Tahoma",
+                "color": "#bfbfbf"
+            }
+        },
+        "edges": {
+            "width": 1,
+            "selectionWidth": 1,
+            "color": {
+                "color": "#ffffff2f", 
+                "highlight": "#FFF",
+            },
+            "smooth": {
+                "type": "continuous",
+            }
+        }
+    }
+    return { "nodes" : nodes, "edges" : edges, "options" : options }, 200
+
+@pluginPages.route("/scan/<scanName>/")
+def getScan(scanName):
+    scansResults = inga._inga().query(api.g.sessionData,query={ "scanName" : scanName, "up" : True },fields=["scanName","ip","up","lastScan","ports"])["results"]
+    return render_template("ingaScan.html",CSRF=jimi.api.g.sessionData["CSRF"],scanResults=ui.dictTable(scansResults))
+
+@pluginPages.route("scan/<scanName>/barScanResults/",methods=["POST"])
+def barScanResults(scanName):
+    bar = ui.bar()
+    with open(Path("plugins/inga/web/includes/sankeyMapping.json")) as json_file:
+        serviceMap = json.load(json_file)
+    bar.addLabel("Other")
+    for service in serviceMap.keys():
+        bar.addLabel(service)
+    scansResults = inga._inga().getAsClass(api.g.sessionData,query={ "scanName" : scanName, "up" : True },fields=["scanName","ip","up","lastScan","ports"])
+    data = {}
+    for scansResult in scansResults:
+        if scansResult.scanName not in data:
+            data[scansResult.scanName] = { "Other" : 0 }
+            for service in serviceMap.keys():
+                data[scansResult.scanName][service] = 0
+        for port in scansResult.ports["tcp"]:
+            matched = getPortMapping(serviceMap,str(port["port"]))
+            if matched != None:
+                data[scansResult.scanName][matched] += 1
+            else:
+                data[scansResult.scanName]["Other"] += 1
+    for scan in data:
+        dataList = []
+        for k,v in data[scan].items():
+            dataList.append(v)
+        bar.addDataset(scan,dataList)
+
+    data = json.loads(jimi.api.request.data)
+    return bar.generate(data), 200
+
+@pluginPages.route("scan/<scanName>/timelineScansResults/",methods=["GET"])
+def timelineScansResults(scanName):
+    scanResults = inga._inga().getAsClass(api.g.sessionData,query={ "scanName" : scanName, "up" : True },fields=["scanName","ip","up","lastScan","ports"])
+    timeline = []
+    for scanResult in scanResults:
+        formatted_date = time.strftime('%Y-%m-%d %H:%M:%S',  time.localtime(scanResult.lastScan))
+        timeline.append({ "id" : len(timeline), "content" : scanResult.ip, "start" : formatted_date })
+
+    return { "timeline" : timeline }, 200
+
+# @pluginPages.route("/scan/<scanName>/")
+# def getScan(scanName):
+#     results             = inga._inga().query(api.g.sessionData,query={ "scanName" : scanName, "up" : True },fields=["scanName","ip","up","lastScan","ports"])["results"]
+#     barChartData        = { }
+#     scanData            = []
+#     networkChartPorts   = []
+#     # 
+#     # New
+#     for scan in results:
+#         c               = 0
+#         portValues      = [ ]
+
+#         try:
+#             tcpPorts        = scan["ports"]["tcp"]
+#             udpPorts        = scan["ports"]["udp"]
+#             combinedPorts   = tcpPorts + udpPorts      
+
+#             for portValue in combinedPorts:
+#                 if portValue["state"] == "open":
+#                     c += 1
+#                     portValues.append( { "port":  portValue["port"], "service": portValue["service"], "state": portValue["state"] })
+#                     networkChartPorts.append([scan["ip"],str(portValue["port"])])
+#             if c > 0:
+#                 try:
+#                     barChartData[scan["ip"]] = c
+#                 except KeyError:
+#                     pass            
+#         except KeyError:
+#             pass
         
-        scanData    = inga._inga().query(api.g.sessionData,query={ "scanName" : scan, "up" : True })["results"]
-        totalCount  = inga._inga().count(api.g.sessionData,query={ "scanName" : scan })["results"][0]["count"]
-        upCount     = inga._inga().count(api.g.sessionData,query={ "scanName" : scan, "up" : True })["results"][0]["count"]
+#         scanData.append( { "scanName": scan["scanName"], "ip": scan["ip"], "up": scan["up"], "lastScan": scan["lastScan"], "portData": portValues } )
 
-        
+#     barChartColours = genRandomColours(len(barChartData))
 
-        for upHost in scanData:
-            if "cidr" in upHost and upHost["cidr"] != "":
-                    # print(upHost)
-                    tcpPorts = upHost["ports"]["tcp"]
-                    udpPorts = upHost["ports"]["udp"]
-
-                    try:
-                        for portValue in tcpPorts:
-                            if portValue["port"] not in portsPieChart:
-                                portsPieChart[portValue["port"]] = 0
-                            if portValue["state"] == "open":
-                                portsPieChart[portValue["port"]]+=1
-                    except KeyError:
-                        pass  
-
-
-                    #merge both lists together
-                    combinedPorts = tcpPorts + udpPorts
-                    for port in combinedPorts:
-                        if port["state"] == "open":
-                            portNum =  port["port"]
-                            matched =   getPortMapping(jsonMap_data,str(portNum))
-                            # print(matched)
-                            if matched != None:
-                                sankeyDict[matched] += 1
-                            else:
-                                sankeyDict["otherPort"] += 1
-
-        for k,v in sankeyDict.items():
-            if v > 0:
-                scanName    = scanData[0]["scanName"]
-                sankeyChartData.append([ scanName, k, v ])
+#     test    = inga._inga().query(api.g.sessionData,query={ "scanName" : scanName, "up" : True },fields=["ports","ip","domains"])["results"]
+#     test2   = []
+#     ids     = []
+#     print(scanName)
+#     for scan in test:
+#         # print(scan)
+#         try:
+#             for portValue in scan["ports"]["tcp"]:
+#                 try:
+#                     ids.append(db.ObjectId(portValue["data"]["webScreenShot"]["storageID"]))
+#                     test2.append({ "url" : "{0}://{1}:{2}".format(portValue["data"]["webServerDetect"]["protocol"],scan["ip"],portValue["port"]), "fileData" : portValue["data"]["webScreenShot"]["storageID"] })
+#                 except KeyError:
+#                     pass
+#             for domainValue in scan["domains"]:
+#                 for protocol in ["http","https"]:
+#                     try:
+#                         ids.append(db.ObjectId(domainValue["data"]["webScreenShot"][protocol]["storageID"]))
+#                         test2.append({ "url" : "{0}://{1}".format(protocol,domainValue["domain"]), "fileData" : domainValue["data"]["webScreenShot"][protocol]["storageID"] })
+#                     except KeyError:
+#                         pass
+#         except KeyError:
+#             pass
+#     print("IDS",ids)
+#     print(test2)
+#     results = storage._storage().query(api.g.sessionData,query={ "_id" : { "$in" : ids } })["results"]
+#     for item in test2:
+#         for storageResult in results:
+#             if item["fileData"] == str(storageResult["_id"]):
+#                 item["fileData"] = storageResult["fileData"]
 
 
-        if totalCount > 0:
-            results.append({ "scanName" : scan, "up" : upCount, "total" : totalCount })
+#     return render_template("ingaScan.html", barChartData=barChartData,barChartColours=barChartColours,scanData=scanData, networkChartPorts=networkChartPorts)    
+#     # return render_template("scan.html", scanResults=results)
 
-    pieChartColours = genRandomColours(len(portsPieChart))
-
-    # print(pieChartColours)
-    # print(portsPieChart)
-    # print(sankeyChartData)
-    return render_template("ingaHomepage.html", scans=results,sankeyPortData=sankeyChartData,pieChart=portsPieChart,pieChartColours=pieChartColours)
-
-@pluginPages.route("/scan/")
-def getScan():
-    scanName            = urllib.parse.unquote_plus(request.args.get("scanName"))
-    results             = inga._inga().query(api.g.sessionData,query={ "scanName" : scanName, "up" : True },fields=["scanName","ip","up","lastScan","ports"])["results"]
-    barChartData        = { }
-    scanData            = []
-    networkChartPorts   = []
-    # 
-    # New
-    for scan in results:
-        c               = 0
-        portValues      = [ ]
-
-        try:
-            tcpPorts        = scan["ports"]["tcp"]
-            udpPorts        = scan["ports"]["udp"]
-            combinedPorts   = tcpPorts + udpPorts      
-
-            for portValue in combinedPorts:
-                if portValue["state"] == "open":
-                    c += 1
-                    portValues.append( { "port":  portValue["port"], "service": portValue["service"], "state": portValue["state"] })
-                    networkChartPorts.append([scan["ip"],str(portValue["port"])])
-            if c > 0:
-                try:
-                    barChartData[scan["ip"]] = c
-                except KeyError:
-                    pass            
-        except KeyError:
-            pass
-        
-        scanData.append( { "scanName": scan["scanName"], "ip": scan["ip"], "up": scan["up"], "lastScan": scan["lastScan"], "portData": portValues } )
-
-    barChartColours = genRandomColours(len(barChartData))
-
-    test    = inga._inga().query(api.g.sessionData,query={ "scanName" : scanName, "up" : True },fields=["ports","ip","domains"])["results"]
-    test2   = []
-    ids     = []
-    print(scanName)
-    for scan in test:
-        # print(scan)
-        try:
-            for portValue in scan["ports"]["tcp"]:
-                try:
-                    ids.append(db.ObjectId(portValue["data"]["webScreenShot"]["storageID"]))
-                    test2.append({ "url" : "{0}://{1}:{2}".format(portValue["data"]["webServerDetect"]["protocol"],scan["ip"],portValue["port"]), "fileData" : portValue["data"]["webScreenShot"]["storageID"] })
-                except KeyError:
-                    pass
-            for domainValue in scan["domains"]:
-                for protocol in ["http","https"]:
-                    try:
-                        ids.append(db.ObjectId(domainValue["data"]["webScreenShot"][protocol]["storageID"]))
-                        test2.append({ "url" : "{0}://{1}".format(protocol,domainValue["domain"]), "fileData" : domainValue["data"]["webScreenShot"][protocol]["storageID"] })
-                    except KeyError:
-                        pass
-        except KeyError:
-            pass
-    print("IDS",ids)
-    print(test2)
-    results = storage._storage().query(api.g.sessionData,query={ "_id" : { "$in" : ids } })["results"]
-    for item in test2:
-        for storageResult in results:
-            if item["fileData"] == str(storageResult["_id"]):
-                item["fileData"] = storageResult["fileData"]
-
-
-    return render_template("ingaScan.html", barChartData=barChartData,barChartColours=barChartColours,scanData=scanData, networkChartPorts=networkChartPorts)    
-    # return render_template("scan.html", scanResults=results)
-
-@pluginPages.route("/scan/images/")
-def getScanImages():
-    scanName = urllib.parse.unquote_plus(request.args.get("scanName"))
+@pluginPages.route("/scan/<scanName>/images/")
+def getScanImages(scanName):
     results = inga._inga().query(api.g.sessionData,query={ "scanName" : scanName, "up" : True },fields=["ports","ip","domains"])["results"]
     result = []
     ids = []
